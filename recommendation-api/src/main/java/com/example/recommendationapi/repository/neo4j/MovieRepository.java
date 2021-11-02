bc7ad0a1cbee530b62c0893c115bcbcaabc73127
Elias Frykholm <elias.frykholm@cygni.se>
2021-11-02 16:52:36 +0100

    Add collaborative filtering query for movies

--- recommendation-api/src/main/java/com/example/recommendationapi/repository/neo4j/MovieRepository.java
+++ recommendation-api/src/main/java/com/example/recommendationapi/repository/neo4j/MovieRepository.java
@@ -4,7 +4,17 @@ import com.example.recommendationapi.models.Movie;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
 
+import java.util.Collection;
+
 public interface MovieRepository extends Neo4jRepository<Movie, String> {
     @Query("MERGE (n:Movie {tmdbId: $tmdbId}) RETURN n")
     Movie addMovie(String tmdbId);
+
+    @Query("MATCH (user:User {userId: $userId})-[myRate:RATED]->()<-[theirRate:RATED]-(similarUser:User)-[:RATED {liked: true}]->(potentialMovie:Movie) " +
+            "WHERE myRate.liked = theirRate.liked " +
+            "AND NOT (user)-[:RATED]->(potentialMovie) " +
+            "RETURN potentialMovie, count(similarUser) as frequency " +
+            "ORDER BY frequency DESC " +
+            "LIMIT 5")
+    Collection<Movie> getMovieRecommendations(String userId);
 }

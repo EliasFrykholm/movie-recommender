1557555adecd4a6fe15e0a4720cefe1dd06103e2
Elias Frykholm <elias.frykholm@cygni.se>
2021-11-03 07:34:01 +0100

    Init moviedata-api

---
+++ moviedata-api/src/main/java/com/example/moviedataapi/MoviedataApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviedataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviedataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviedataApiApplication.class, args);
+	}
+
+}

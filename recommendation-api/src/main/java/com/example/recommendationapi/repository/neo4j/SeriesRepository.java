92eefd9d30b0f7676014b09555fb3ec21301e056
Elias Frykholm <elias.frykholm@cygni.se>
2021-11-03 07:22:12 +0100

    Add series repo

---
+++ recommendation-api/src/main/java/com/example/recommendationapi/repository/neo4j/SeriesRepository.java
@@ -0,0 +1,21 @@
+package com.example.recommendationapi.repository.neo4j;
+
+import com.example.recommendationapi.models.Movie;
+import com.example.recommendationapi.models.Series;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+
+import java.util.Collection;
+
+public interface SeriesRepository extends Neo4jRepository<Series, String> {
+    @Query("MERGE (n:Series {tmdbId: $tmdbId}) RETURN n")
+    Movie addSeries(String tmdbId);
+
+    @Query("MATCH (user:User {userId: $userId})-[myRate:RATED]->()<-[theirRate:RATED]-(similarUser:User)-[:RATED {liked: true}]->(potentialSeries:Series) " +
+            "WHERE myRate.liked = theirRate.liked " +
+            "AND NOT (user)-[:RATED]->(potentialSeries) " +
+            "RETURN potentialSeries, count(similarUser) as frequency " +
+            "ORDER BY frequency DESC " +
+            "LIMIT 5")
+    Collection<Series> getSeriesRecommendations(String userId);
+}
